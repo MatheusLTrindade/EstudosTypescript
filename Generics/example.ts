98252188343437b3304f9496a346ead81637d57d
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-06-15 23:33:59 -0300

    add Generics

---
+++ Generics/example.ts
@@ -0,0 +1,13 @@
+function first(array) {
+    return array[0]
+}
+
+function last<ArrayType>(array: ArrayType[]): ArrayType | undefined {
+    return array[array.length - 1]
+}
+
+const pilots = ['Luke', 'Biggs', 'Wedge', 'Han', 'Lando']
+
+const firstPilot = first(pilots)
+
+const lastPilot = last(pilots)
